455f5db538ec9ff6429744e29e1f7476901fc564
wangt24 <wangt24@ziroom.com>
2020-12-26 17:46:57 +0800

    网关转发商品服务和后台管理系统服务

--- tmall-gateway/src/main/java/com/wt/tmall/gateway/TmallGatewayApplication.java
+++ tmall-gateway/src/main/java/com/wt/tmall/gateway/TmallGatewayApplication.java
@@ -1,11 +1,10 @@
 package com.wt.tmall.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
