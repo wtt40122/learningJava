15ba55ee8d1e6d51f1b32b6bafe86ae2b046965e
wangtao <wangtao39@xiaomi.com>
2022-01-23 21:09:16 +0800

    netty websocket server

---
+++ mini-rpc/src/main/java/com/wt/netty/websocker/WebSocketServer.java
@@ -0,0 +1,56 @@
+package com.wt.netty.websocker;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2022/1/23 20:45
+ */
+public class WebSocketServer {
+
+    public void run(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast("http-codes", new HttpServerCodec());
+                            pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+                            pipeline.addLast("http-chunked", new ChunkedWriteHandler());
+                            pipeline.addLast("socket-handler", new WebSocketServerHandler());
+                        }
+                    });
+            Channel channel = bootstrap.bind(port).sync().channel();
+            System.out.println("Web socket server started at port" + port + ".");
+            System.out.println("Open you browser and navigate to http://localhost:" + port + "/");
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new WebSocketServer().run(port);
+    }
+}
---
+++ mini-rpc/src/main/java/com/wt/netty/xml/pojo/Shipping.java
@@ -0,0 +1,11 @@
+package com.wt.netty.xml.pojo;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2022/1/20 20:22
+ */
+public enum Shipping {
+    STANDARD_MAIL, PRIORITY_MAIL, INTERNATIONAL_MAIL, DOMESTIC_EXPRESS, INTERNATIONAL_EXPRESS
+}
