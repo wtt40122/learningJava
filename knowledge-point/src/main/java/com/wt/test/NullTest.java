080e282fec6a6f51f1177452af5c0a0c8562d728
wtt <1136220284@qq.com>
2022-04-13 00:33:29 +0800

    贪心算法硬币找零问题

---
+++ knowledge-point/src/main/java/com/wt/test/NullTest.java
@@ -0,0 +1,21 @@
+package com.wt.test;
+
+/**
+ * @Auther: wtt
+ * @Date: 2022/1/15 17:32
+ * @Description:
+ */
+public class NullTest {
+    public static void main(String[] args) {
+        int age = 0;
+        System.out.println("user age:" + age);
+
+        long money;
+        money = 10L;
+        System.out.println("user money" + money);
+
+        String name = null;
+        System.out.println("user name:" + name);
+
+    }
+}
