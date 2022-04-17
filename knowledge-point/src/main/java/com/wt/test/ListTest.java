e4f48f23caf117a2bde491eaabd4ee910cd5aa83
wtt <1136220284@qq.com>
2022-04-17 21:22:09 +0800

    update

--- knowledge-point/src/main/java/com/wt/test/ListTest.java
+++ knowledge-point/src/main/java/com/wt/test/ListTest.java
@@ -1,12 +1,18 @@
 package com.wt.test;
 
 import com.wt.algorithm.leetCode.ListNode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Auther: wtt
  * @Date: 2021/5/11 20:41
  * @Description:
  */
+@Slf4j
 public class ListTest {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
@@ -20,6 +26,15 @@ public class ListTest {
         }
         return false;
 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List list = Arrays.asList(arr);
+        System.out.println(String.format("list:%s size:%s class:%s", list, list.size(), list.get(0).getClass()));
+        int[] arr1 = {1, 2, 3};
+        List list1 = Arrays.stream(arr1).boxed().collect(Collectors.toList());
+        System.out.println(String.format("list:%s size:%s class:%s", list1, list1.size(), list1.get(0).getClass()));
     }
 
 }
