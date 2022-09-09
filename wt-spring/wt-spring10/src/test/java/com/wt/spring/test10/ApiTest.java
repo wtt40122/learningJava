c82d1f1a9d09b2be62870f9fed2f54f8ccf90439
wtt <1136220284@qq.com>
2022-09-09 16:26:23 +0800

    update

---
+++ wt-spring/wt-spring10/src/test/java/com/wt/spring/test10/ApiTest.java
@@ -0,0 +1,105 @@
+package com.wt.spring.test10;
+
+import com.wt.spring.beans.factory.config.BeanFactoryPostProcessor;
+import com.wt.spring.beans.factory.config.BeanPostProcessor;
+import com.wt.spring.beans.factory.support.DefaultListableBeanFactory;
+import com.wt.spring.beans.factory.xml.XmlBeanDefinitionReader;
+import com.wt.spring.context.support.ClassPathXmlApplicationContext;
+import com.wt.spring.test10.bean.UserService;
+import com.wt.spring.test10.common.MyBeanFactoryPostProcessor;
+import com.wt.spring.test10.common.MyBeanPostProcessor;
+import org.junit.Test;
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @author: wtt
+ * @date: 2022/9/4 19:11
+ * @description:
+ */
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactoryPostProcessorAndBeanPostProcessor() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+//        beanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
+        beanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
+        BeanFactoryPostProcessor beanFactoryPostProcessor = new MyBeanFactoryPostProcessor();
+        beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
+
+        BeanPostProcessor beanPostProcessor = new MyBeanPostProcessor();
+        beanFactory.addBeanPostProcessor(beanPostProcessor);
+
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+
+    }
+
+    @Test
+    public void test_xml() {
+        ClassPathXmlApplicationContext applicationContext = new
+                ClassPathXmlApplicationContext("classpath:spring.xml");
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+    }
+
+    @Test
+    public void test_xml_new() {
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        System.out.println("result:" + userService.queryUserInfo());
+    }
+
+    @Test
+    public void test_xml_aware() {
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("result:" + result);
+        System.out.println("applicationContextAware:" + userService.getApplicationContext());
+        System.out.println("beanFactoryAware:" + userService.getBeanFactory());
+    }
+
+    @Test
+    public void test_prototype() {
+        ClassPathXmlApplicationContext applicationContext = new
+                ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        UserService userService01 = applicationContext.getBean("userService", UserService.class);
+        UserService userService02 = applicationContext.getBean("userService", UserService.class);
+
+        System.out.println(userService01);
+        System.out.println(userService02);
+
+        // 打印16进制hash
+        System.out.println(userService01 + " 十六进制哈希:" + Integer.toHexString(userService01.hashCode()));
+        System.out.println(ClassLayout.parseInstance(userService01).toPrintable());
+    }
+
+    @Test
+    public void test_factory_bean() {
+        ClassPathXmlApplicationContext applicationContext = new
+                ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        System.out.println("result:" + userService.queryUserInfo());
+    }
+
+
+    @Test
+    public void test_hook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("close")));
+    }
+}
---
+++ wt-spring/wt-spring10/src/test/java/com/wt/spring/test10/bean/UserService.java
@@ -0,0 +1,106 @@
+package com.wt.spring.test10.bean;
+
+import com.wt.spring.beans.BeansException;
+import com.wt.spring.beans.factory.*;
+import com.wt.spring.context.ApplicationContext;
+import com.wt.spring.context.ApplicationContextAware;
+
+/**
+ * @author: wtt
+ * @date: 2022/9/4 19:12
+ * @description:
+ */
+public class UserService implements InitializingBean, DisposableBean,
+        BeanNameAware, BeanClassLoaderAware, ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+    private BeanFactory beanFactory;
+
+    private String uId;
+    private String company;
+    private String location;
+    private UserDao userDao;
+    private IUserDao iUserDao;
+
+    public IUserDao getiUserDao() {
+        return iUserDao;
+    }
+
+    public void setiUserDao(IUserDao iUserDao) {
+        this.iUserDao = iUserDao;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("执行：UserService.destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("执行：UserService.afterPropertiesSet");
+    }
+
+    public String queryUserInfo() {
+        return userDao.queryUserName(uId) + "," + company + "," + location + "," + iUserDao.queryUserName(uId);
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void setBeanName(String name) throws BeansException {
+        System.out.println("Bean Name is：" + name);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader beanClassLoader) throws BeansException {
+        System.out.println("ClassLoader：" + beanClassLoader);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+}
