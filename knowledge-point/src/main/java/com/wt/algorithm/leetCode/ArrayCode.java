9dda5c666327457571ccf8bc3d40f64ac09a840a
wtt <wangtao@xiaomi.com>
2022-11-16 21:35:18 +0800

    数组移除元素

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/ArrayCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/ArrayCode.java
@@ -44,7 +44,40 @@ public class ArrayCode {
         return -1;
     }
 
+    public static int removeElement(int[] nums, int val) {
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == val) {
+                for (int j = i + 1; j < size; j++) {
+                    nums[j - 1] = nums[j];
+                }
+                i--;
+                size--;
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 双指针法
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement1(int[] nums, int val) {
+        int slowIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (val != nums[i]) {
+                nums[slowIndex] = nums[i];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
+    }
+
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[]{-1,0,3,5,9,12}, 0));
+//        System.out.println(binarySearch(new int[]{-1, 0, 3, 5, 9, 12}, 0));
+        System.out.println(removeElement1(new int[]{-1, 3, 3, 5, 9, 12, 3}, 3));
     }
 }
