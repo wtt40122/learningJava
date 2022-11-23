cbfd11041042044bbbc4b8bd5d5b023eabcd1b7a
wtt <wangtao@xiaomi.com>
2022-11-23 14:03:18 +0800

    三数之和

---
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/MapTestCode.java
@@ -0,0 +1,142 @@
+package com.wt.algorithm.leetCode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2022/11/22 10:22
+ */
+public class MapTestCode {
+
+    public static boolean isYiWei(String s, String t) {
+        int[] hash = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            hash[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        List<Integer> list = new ArrayList<>();
+        if (set1.size() < set2.size()) {
+            list = set1.stream().filter(set2::contains).collect(Collectors.toList());
+        } else {
+            list = set2.stream().filter(set1::contains).collect(Collectors.toList());
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    private static Integer getNextNumber(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int ys = n % 10;
+            n = n / 10;
+            sum += ys * ys;
+        }
+        return sum;
+    }
+
+
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int sum = i + j;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        for (int i : nums3) {
+            for (int j : nums4) {
+                int sum = i + j;
+                if (map.containsKey(0 - sum)) {
+                    count += map.get(0 - sum);
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (char c : ransomNote.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        for (char c : magazine.toCharArray()) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+        for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
+            if (!map2.containsKey(entry.getKey()) || !map2.get(entry.getKey()).equals(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 三数之和
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return list;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isYiWei("rat", "car"));
+        System.out.println(getNextNumber(123));
+    }
+}
