8213878b65b5c0aa3d86073ea19443abc8375468
wtt <wangtao@xiaomi.com>
2022-11-24 20:03:00 +0800

    字符串算法code

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/StringCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/StringCode.java
@@ -38,4 +38,138 @@ public class StringCode {
             chars[j] = temp;
         }
     }
+
+
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String replaceSpaceDp(String s) {
+        char[] chars = s.toCharArray();
+        int count = 0;
+        for (char c : chars) {
+            if (c == ' ') {
+                count++;
+            }
+        }
+        char[] newChars = new char[s.length() + 2 * count];
+        int oldRight = s.length() - 1;
+        int newRight = newChars.length - 1;
+        while (oldRight >= 0) {
+            if (chars[oldRight] == ' ') {
+                newChars[newRight] = '0';
+                newChars[--newRight] = '2';
+                newChars[--newRight] = '%';
+            } else {
+                newChars[newRight] = chars[oldRight];
+            }
+            newRight--;
+            oldRight--;
+        }
+        return new String(newChars);
+    }
+
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] strs = s.split(" ");
+        for (int i = strs.length - 1; i >= 0; i--) {
+            if ((i == 0 || i == s.length() - 1) && s.charAt(i) == ' ') {
+                continue;
+            }
+            if (strs[i].equals("")) {
+                continue;
+            } else {
+                sb.append(" " + strs[i]);
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    /**
+     * 另一种解法
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseWords2(String s) {
+        // 移除前后空格及中间多余的空格
+        StringBuilder removeSpace = removeSpace(s);
+        // 翻转字符串
+        reverseStr(removeSpace, 0, removeSpace.length() - 1);
+        // 反转每个字符串
+        reversePerWord(removeSpace);
+        return removeSpace.toString();
+    }
+
+    public static void reversePerWord(StringBuilder sb) {
+        int slow = 0;
+        int fast = 0;
+        while (slow < sb.length()) {
+            while (fast < sb.length() && sb.charAt(fast) != ' ') {
+                fast++;
+            }
+            reverseStr(sb, slow, fast - 1);
+            slow = fast + 1;
+            fast = slow + 1;
+        }
+    }
+
+    public static void reverseStr(StringBuilder sb, int start, int end) {
+        while (start < end) {
+            char temp = sb.charAt(end);
+            sb.setCharAt(end, sb.charAt(start));
+            sb.setCharAt(start, temp);
+            start++;
+            end--;
+        }
+    }
+
+    public static StringBuilder removeSpace(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (s.charAt(start) == ' ') start++;
+        while (s.charAt(end) == ' ') end--;
+        StringBuilder sb = new StringBuilder();
+        while (start <= end) {
+            if (s.charAt(start) != ' ' || sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(s.charAt(start));
+            }
+            start++;
+        }
+        return sb;
+    }
+
+    public static String reverseLeftWords(String s, int n) {
+        if (s.length() < n) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.substring(n));
+        sb.append(s, 0, n);
+        return sb.toString();
+    }
+
+    public static String reverseLeftWords2(String s, int n) {
+        StringBuilder sb = new StringBuilder(s);
+        reverseStr(sb, 0, n - 1);
+        reverseStr(sb, n, s.length() - 1);
+        reverseStr(sb, 0, s.length() - 1);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(replaceSpaceDp("We are happy."));
+//        System.out.println(reverseWords2("a good   example"));
+//        System.out.println(removeSpace("a good   example"));
+        System.out.println(reverseLeftWords2("lrloseumgh", 6));
+    }
 }
