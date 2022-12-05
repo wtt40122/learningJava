4ba547af353d81b662b44e4cbec91ce0a147d858
wtt <wangtao@xiaomi.com>
2022-12-05 10:13:02 +0800

    二叉树code啊

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/TreeCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/TreeCode.java
@@ -760,12 +760,480 @@ public class TreeCode {
         return res;
     }
 
+    /**
+     * 左叶子节点之和
+     *
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if (null == root || (null == root.left && null == root.right)) {
+            return sum;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode treeNode = stack.pop();
+            if (null != treeNode.left && treeNode.left.left == null && treeNode.left.right == null) {
+                sum += treeNode.left.val;
+            }
+            if (treeNode.right != null) {
+                stack.push(treeNode.right);
+            }
+            if (treeNode.left != null) {
+                stack.push(treeNode.left);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 左叶子节点之和递归法
+     *
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeavesCur(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        if (null == root.left && null == root.right) {
+            return 0;
+        }
+        int leftNum = sumOfLeftLeavesCur(root.left);
+        if (null != root.left && null == root.left.left && null == root.left.right) {
+            leftNum += root.left.val;
+        }
+        int rightNum = sumOfLeftLeavesCur(root.right);
+        return leftNum + rightNum;
+    }
+
+    /**
+     * 做叶子节点层序法
+     *
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeavesLay(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        if (null == root.left && null == root.right) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode treeNode = queue.poll();
+                if (null != treeNode.left && treeNode.left.left == null && treeNode.left.right == null) {
+                    sum += treeNode.left.val;
+                }
+                if (null != treeNode.left) {
+                    queue.offer(treeNode.left);
+                }
+                if (null != treeNode.right) {
+                    queue.offer(treeNode.right);
+                }
+                size--;
+            }
+        }
+        return sum;
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int index = size;
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (size == index) {
+                    result = node.val;
+                }
+                if (null != node.left) {
+                    queue.offer(node.left);
+                }
+                if (null != node.right) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+        }
+        return result;
+    }
+
+    int maxDepth = -1;
+    int result = 0;
+
+    public int findBottomLeftValueRecur(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        transfer(root, 0);
+        return result;
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (null == root) {
+            return false;
+        }
+        return transfer(root, targetSum - root.val);
+    }
+
+    private boolean transfer(TreeNode treeNode, int count) {
+        if (null == treeNode.left && null == treeNode.right) {
+            return count == 0;
+        }
+        if (null != treeNode.left && transfer(treeNode.left, count - treeNode.left.val)) {
+            return true;
+        }
+        if (null != treeNode.right && transfer(treeNode.right, count - treeNode.right.val)) {
+            return true;
+        }
+        return false;
+    }
+
+    List<List<Integer>> list = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if (null == root) {
+            return list;
+        }
+        List<Integer> nodeList = new ArrayList<>();
+        transfer(root, targetSum - root.val, nodeList);
+        return list;
+    }
+
+    private void transfer(TreeNode treeNode, int count, List<Integer> nodes) {
+        nodes.add(treeNode.val);
+        if (null == treeNode.left && null == treeNode.right) {
+            if (count == 0) {
+                list.add(new ArrayList<>(nodes));
+            }
+        }
+        if (null != treeNode.left) {
+            transfer(treeNode.left, count - treeNode.left.val, nodes);
+            nodes.remove(nodes.size() - 1);
+        }
+        if (null != treeNode.right) {
+            transfer(treeNode.right, count - treeNode.right.val, nodes);
+            nodes.remove(nodes.size() - 1);
+        }
+    }
+
+    /**
+     * 从中序与后序遍历序列构造二叉树
+     *
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (postorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        if (postorder.length == 1) {
+            return root;
+        }
+        int index;
+        for (index = 0; index < inorder.length; index++) {
+            if (root.val == inorder[index]) {
+                break;
+            }
+        }
+        // 切割中序数组
+        int[] leftInorder = splitArray(inorder, 0, index);
+        int[] rightInorder = splitArray(inorder, index + 1, inorder.length);
+
+        // 切割后序数组
+        int[] leftPostorder = splitArray(postorder, 0, leftInorder.length);
+        int[] rightPostorder = splitArray(postorder, leftInorder.length, postorder.length - 1);
+        root.left = buildTree(leftInorder, leftPostorder);
+        root.right = buildTree(rightInorder, rightPostorder);
+        return root;
+    }
+
+    private static int[] splitArray(int[] array, int start, int end) {
+        int[] newArray = new int[end - start];
+        for (int i = start; i < end; i++) {
+            newArray[i - start] = array[i];
+        }
+        return newArray;
+    }
+
+    public TreeNode buildTreePi(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        if (preorder.length == 1) {
+            return root;
+        }
+        int index;
+        for (index = 0; index < inorder.length; index++) {
+            if (preorder[0] == inorder[index]) {
+                break;
+            }
+        }
+        //切割中序
+        int[] leftInorder = splitArray(inorder, 0, index);
+        int[] rightInorder = splitArray(inorder, index + 1, inorder.length);
+        //切割前序
+        int[] leftPreorder = splitArray(preorder, 1, leftInorder.length + 1);
+        int[] rightPreorder = splitArray(preorder, leftInorder.length + 1, preorder.length);
+        root.left = buildTreePi(leftPreorder, leftInorder);
+        root.right = buildTreePi(rightPreorder, rightInorder);
+        return root;
+    }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        int maxIndex = 0;
+        int maxResult = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxResult) {
+                maxResult = nums[i];
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxResult);
+        if (nums.length == 1) {
+            return root;
+        }
+        //切割数据
+        int[] leftNums = splitArray(nums, 0, maxIndex);
+        int[] rightNums = splitArray(nums, maxIndex + 1, nums.length);
+        root.left = constructMaximumBinaryTree(leftNums);
+        root.right = constructMaximumBinaryTree(rightNums);
+        return root;
+    }
+
+    public TreeNode constructMaximumBinaryTreeOpt(int[] nums) {
+        return construct(nums, 0, nums.length);
+    }
+
+    public TreeNode construct(int[] nums, int left, int right) {
+        if (left >= right) {
+            return null;
+        }
+        int maxIndex = 0;
+        int maxValue = 0;
+        for (int i = left; i < right; i++) {
+            if (nums[i] > maxValue) {
+                maxIndex = i;
+                maxValue = nums[i];
+            }
+        }
+        TreeNode root = new TreeNode(maxValue);
+        if (right - left == 1) {
+            return root;
+        }
+        root.left = construct(nums, left, maxIndex);
+        root.right = construct(nums, maxIndex + 1, right);
+        return root;
+    }
+
+    /**
+     * 合并二叉树
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (null == root1) {
+            return root2;
+        }
+        if (null == root2) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (null == root) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        innerTree(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void innerTree(TreeNode treeNode, List<Integer> list) {
+        if (null == treeNode) {
+            return;
+        }
+        innerTree(treeNode.left, list);
+        list.add(treeNode.val);
+        innerTree(treeNode.right, list);
+    }
+
+    long maxValue = Long.MIN_VALUE;
+
+    public boolean isValidBST1(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        boolean left = isValidBST1(root.left);
+        if (root.val > maxValue) {
+            maxValue = root.val;
+        } else {
+            return false;
+        }
+        boolean right = isValidBST1(root.right);
+        return left && right;
+    }
+
+    TreeNode pre = null;
+
+    public boolean isValidBST2(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        boolean left = isValidBST2(root.left);
+        if (null != pre && pre.val > root.val) {
+            return false;
+        }
+        pre = root;
+        boolean right = isValidBST2(root.right);
+        return left && right;
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        innerTree(root, list);
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            int abs = Math.abs(list.get(i) - list.get(i - 1));
+            if (abs < minValue) {
+                minValue = abs;
+            }
+        }
+        return minValue;
+    }
+
+    int minValue = Integer.MAX_VALUE;
+
+    public int getMinimumDifference1(TreeNode root) {
+        transfer(root);
+        return minValue;
+    }
+
+    private void transfer(TreeNode cur) {
+        if (null == cur) {
+            return;
+        }
+        transfer(cur.left);
+        if (null != pre) {
+            minValue = Math.min(minValue, cur.val - pre.val);
+        }
+        pre = cur;
+        transfer(cur.right);
+    }
+
+    /**
+     * 二叉搜索树求众数
+     *
+     * @param root
+     * @return
+     */
+    int count;
+    int maxCount;
+    List<Integer> valList = new ArrayList<>();
+
+    public int[] findMode(TreeNode root) {
+        transferF(root);
+        int[] result = new int[valList.size()];
+        for (int i = 0; i < valList.size(); i++) {
+            result[i] = valList.get(i);
+        }
+        return result;
+    }
+
+    private void transferF(TreeNode cur) {
+        if (null == cur) {
+            return;
+        }
+        transferF(cur.left);
+        if (null == pre) {
+            count = 1;
+        } else if (pre.val == cur.val) {
+            count++;
+        } else {
+            count = 1;
+        }
+        pre = cur;
+        if (count == maxCount) {
+            valList.add(cur.val);
+        }
+        if (count > maxCount) {
+            valList.clear();
+            valList.add(cur.val);
+            maxCount = count;
+        }
+        transferF(cur.right);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (null != left && null != right) {
+            return root;
+        } else if (null == left && null != right) {
+            return right;
+        } else if (null != left && null == right) {
+            return left;
+        } else {
+            return null;
+        }
+    }
+
+
     public static void main(String[] args) {
-        TreeNode treeNode4 = new TreeNode(4);
-        TreeNode treeNode3 = new TreeNode(3, treeNode4, null);
-        TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode1 = new TreeNode(1, treeNode2, treeNode3);
-        List<Integer> list = rightSideView(treeNode1);
-        list.stream().forEach(System.out::println);
+//        TreeNode treeNode4 = new TreeNode(4);
+//        TreeNode treeNode3 = new TreeNode(3, treeNode4, null);
+//        TreeNode treeNode2 = new TreeNode(2);
+//        TreeNode treeNode1 = new TreeNode(1, treeNode2, treeNode3);
+//        List<Integer> list = rightSideView(treeNode1);
+//        list.stream().forEach(System.out::println);
+        int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int[] ints = splitArray(array, 2, 8);
+        Arrays.stream(ints).forEach(System.out::println);
     }
 }
