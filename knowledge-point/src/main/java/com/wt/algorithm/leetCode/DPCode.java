9811451fc4e5e7d00da84ec05ac9e2e6ad75876f
wtt <wangtao@xiaomi.com>
2022-12-20 10:40:45 +0800

    动态规划算法（爬楼梯、不同路径）

---
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/DPCode.java
@@ -0,0 +1,110 @@
+package com.wt.algorithm.leetCode;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description 动态规划算法
+ * 动态规划5步曲
+ * 1。dp数确定组以及下标的含义
+ * 2.确定递推公示
+ * 3.dp数组如何初始化
+ * 4.确定遍历顺序
+ * 5.举例推导dp数组
+ * @date 2022/12/19 10:51
+ */
+public class DPCode {
+
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public int fib2(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int fib3(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int a = 0, b = 1, c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    /**
+     * 爬楼梯
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+
+    /**
+     * 使用最小花费爬楼梯
+     *
+     * @param cost
+     * @return
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        if (cost.length < 2) {
+            return 0;
+        }
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];
+    }
+
+    /**
+     * 不同路径
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
