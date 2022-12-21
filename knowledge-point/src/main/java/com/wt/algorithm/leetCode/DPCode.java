cbe367710ec308d07043cbc1471529de7ef4f38c
wtt <wangtao@xiaomi.com>
2022-12-21 15:04:13 +0800

    01背包问题

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/DPCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/DPCode.java
@@ -137,4 +137,38 @@ public class DPCode {
         }
         return dp[m - 1][n - 1];
     }
+
+    /**
+     * 背包问题
+     *
+     * @param weight
+     * @param value
+     * @param bagSize
+     * @return
+     */
+    public int BagProblem(int[] weight, int[] value, int bagSize) {
+        int goods = weight.length;
+        int[][] dp = new int[goods][bagSize + 1];
+        for (int j = weight[0]; j <= bagSize; j++) {
+            dp[0][j] = value[0];
+        }
+        for (int i = 1; i < goods; i++) {
+            for (int j = 1; j <= bagSize; j++) {
+                if (weight[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+                }
+            }
+        }
+        return dp[goods-1][bagSize];
+    }
+
+    public static void main(String[] args) {
+        DPCode dpCode = new DPCode();
+        int[] weight = {1, 3, 4};
+        int[] value = {15, 20, 30};
+        int bagSize = 4;
+        System.out.println(dpCode.BagProblem(weight, value, bagSize));
+    }
 }
