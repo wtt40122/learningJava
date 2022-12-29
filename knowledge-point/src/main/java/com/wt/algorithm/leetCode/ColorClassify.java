fa391e1a3bdfb790327069e2ab3429baa66e518c
wtt <wangtao@xiaomi.com>
2022-12-29 09:58:43 +0800

    链表题目更新

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/ColorClassify.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/ColorClassify.java
@@ -7,9 +7,9 @@ import java.util.Random;
  * @Auther: wtt
  * @Date: 2021/3/15 12:32
  * @Description: 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
- *
+ * <p>
  * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/sort-colors
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -47,5 +47,21 @@ public class ColorClassify {
         int[] nums = {2, 1, 0, 1, 2, 0};
         ColorClassify.classify(nums);
         System.out.println(Arrays.toString(nums));
+    }
+
+    public void sortColors(int[] nums) {
+        int i = -1, k = 0, j = nums.length;
+        while (k < j) {
+            if (nums[k] == 0) {
+                i++;
+                swap(nums, i, k);
+                k++;
+            } else if (nums[k] == 1) {
+                k++;
+            } else {
+                j--;
+                swap(nums, k, j);
+            }
+        }
     }
 }
--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/LinkedListCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/LinkedListCode.java
@@ -1,5 +1,8 @@
 package com.wt.algorithm.leetCode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @author wtt
  * @version 1.0
@@ -50,6 +53,86 @@ public class LinkedListCode {
             cur = temp2;
         }
         return dummyHead.next;
+    }
+
+    ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        while (null != p1 && null != p2) {
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            p = p.next;
+        }
+        if (null != p1) {
+            p.next = p1;
+        }
+        if (null != p2) {
+            p.next = p2;
+        }
+        return dummy.next;
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-2);
+        ListNode p1 = dummy1;
+        ListNode p2 = dummy2;
+        ListNode p = head;
+        while (null != p) {
+            if (p.val < x) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        if (0 == lists.length) {
+            return null;
+        }
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length,
+                Comparator.comparingInt(o -> o.val));
+        for (ListNode head : lists) {
+            if (null != head) {
+                pq.add(head);
+            }
+        }
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            p.next = node;
+            if (null != node.next) {
+                pq.add(node.next);
+            }
+            p = p.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (null != p2 && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
     }
 
     public static void main(String[] args) {
