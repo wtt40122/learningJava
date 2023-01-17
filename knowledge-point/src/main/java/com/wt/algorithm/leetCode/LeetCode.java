f0befe6dd867e16f3ca4ba2af26c057b6fae17a9
wtt <wangtao@xiaomi.com>
2023-01-17 17:07:51 +0800

    回溯算法题目add

--- knowledge-point/src/main/java/com/wt/algorithm/leetCode/LeetCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/leetCode/LeetCode.java
@@ -160,10 +160,129 @@ public class LeetCode {
         }
     }
 
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTrack(n, k, 1, new ArrayList(), result);
+        return result;
+    }
+
+    private void backTrack(int n, int k, int index, List<Integer> path, List<List<Integer>> result) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i <= n; i++) {
+            path.add(i);
+            backTrack(n, k, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /**
+     * 子集II
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backTracking(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backTracking(int[] nums, int index, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        System.out.println(path.stream().map(Object::toString).collect(Collectors.joining(",")));
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            backTracking(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int target, int index, int sum, List<Integer> path, List<List<Integer>> result) {
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            path.add(candidates[i]);
+            backtrack(candidates, target, i + 1, sum + candidates[i], path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, new int[nums.length], new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, int[] used, List<Integer> path, List<List<Integer>> result) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] == 1) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == 0) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = 1;
+            backtrack(nums, used, path, result);
+            used[i] = 0;
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTrackCSM(candidates, target, 0, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backTrackCSM(int[] candidates, int target, int index, int sum, List<Integer> path, List<List<Integer>> result) {
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            backTrackCSM(candidates, target, i, sum + candidates[i], path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         LeetCode code = new LeetCode();
-        int[] nums = {0, 3, 2, 1};
-        code.smallerNumbersThanCurrent(nums);
-        code.validMountainArray(nums);
+        int[] nums = {1, 2, 2};
+//        code.smallerNumbersThanCurrent(nums);
+//        code.validMountainArray(nums);
+        List<List<Integer>> list = code.permuteUnique(nums);
+        System.out.println(list);
+
     }
 }
