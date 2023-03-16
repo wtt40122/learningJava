8997d3e390cc540d0cc4fdc008a7fb839c840a6a
wtt <1136220284@qq.com>
2023-03-16 17:07:09 +0800

    nettyServer receive data

---
+++ netty-learn/netty-02/src/main/java/com/wt/netty02/NettyServer.java
@@ -0,0 +1,41 @@
+package com.wt.netty02;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2023/3/16 16:10
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        new NettyServer().bind(7397);
+    }
+
+    private void bind(int port) {
+        EventLoopGroup parentGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(parentGroup, childGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new MyChannelInitializer());
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            childGroup.shutdownGracefully();
+            parentGroup.shutdownGracefully();
+        }
+    }
+}
