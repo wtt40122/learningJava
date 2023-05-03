e8bbe028dda1f2e0a26751d0070d27140615047f
wtt <1136220284@qq.com>
2023-05-03 11:10:33 +0800

    netty与springboot

---
+++ netty-learn/netty-13/src/main/java/com/wt/netty13/server/ServerChannelInitializer.java
@@ -0,0 +1,29 @@
+package com.wt.netty13.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2023/5/3 10:49
+ */
+public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        // 基于换行符号
+        channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
+        // 在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
