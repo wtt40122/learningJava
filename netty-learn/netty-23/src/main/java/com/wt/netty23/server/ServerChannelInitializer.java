a6cb48f77ad10fd86247d2ccaa82c92fe545dacd
wtt <1136220284@qq.com>
2023-05-07 16:36:22 +0800

    feat:ChunkedStream数据流切块传输

---
+++ netty-learn/netty-23/src/main/java/com/wt/netty23/server/ServerChannelInitializer.java
@@ -0,0 +1,32 @@
+package com.wt.netty23.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+import java.nio.charset.Charset;
+
+/**
+ * @Author: wtt
+ * @Date: 2023/5/7 16:27
+ * @Description:
+ */
+public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        // 基于换行符号
+        channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        // 流量分块
+        channel.pipeline().addLast(new ChunkedWriteHandler());
+        channel.pipeline().addLast(new MyServerChunkHandler());
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+        // 解码转String，注意调整自己的编码格式GBK、UTF-8
+        channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
+        // 在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
