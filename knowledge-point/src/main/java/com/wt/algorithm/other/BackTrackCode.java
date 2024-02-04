ef208732d5b910b5df7bcb888b0bdc6e587217e3
wtt <1136220284@qq.com>
2024-02-04 16:05:59 +0800

    backtracking algorithm arrangement problem code

--- knowledge-point/src/main/java/com/wt/algorithm/other/BackTrackCode.java
+++ knowledge-point/src/main/java/com/wt/algorithm/other/BackTrackCode.java
@@ -162,11 +162,232 @@ public class BackTrackCode {
         }
     }
 
+    /**
+     * 分割回文串
+     *
+     * @param s
+     * @return
+     */
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack5(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack5(String s, int index, List<String> path, List<List<String>> result) {
+        if (index >= s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+            if (isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i + 1));
+                backtrack5(s, i + 1, path, result);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    /**
+     * 复原ip地址
+     *
+     * @param s
+     * @return
+     */
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<>();
+        backtrack6(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack6(String s, int index, List<String> path, List<String> result) {
+        if (index >= s.length()) {
+            if (path.size() == 4) {
+                result.add(String.join(".", path));
+            }
+            return;
+        }
+        for (int i = index; i < s.length(); i++) {
+            if (is_valid_ip(s, index, i + 1)) {
+                path.add(s.substring(index, i + 1));
+                backtrack6(s, i + 1, path, result);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean is_valid_ip(String s, int start, int end) {
+        String numberStr = s.substring(start, end);
+        if ((numberStr.length() > 1 && numberStr.charAt(0) == '0') || numberStr.length() > 4) {
+            return false;
+        }
+        int number = Integer.parseInt(numberStr);
+        if (number > 255) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 子集问题
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack7(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack7(int[] nums, int index, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        if (index == nums.length) {
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack7(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /**
+     * 子集II
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack8(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack8(int[] nums, int index, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        if (index == nums.length) {
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            backtrack8(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /**
+     * 递增子序列
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack9(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack9(int[] nums, int index, List<Integer> path, List<List<Integer>> result) {
+        if (path.size() > 1) {
+            result.add(new ArrayList<>(path));
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (!path.isEmpty() && nums[i] < path.get(path.size() - 1) || set.contains(nums[i])) {
+                continue;
+            }
+            set.add(nums[i]);
+            path.add(nums[i]);
+            backtrack9(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /**
+     * 全排列
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack10(nums, new ArrayList<>(), result, used);
+        return result;
+    }
+
+    private void backtrack10(int[] nums, List<Integer> path, List<List<Integer>> result, boolean[] used) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            set.add(nums[i]);
+            path.add(nums[i]);
+            backtrack10(nums, path, result, used);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    /**
+     * 全排列II
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack11(nums, new ArrayList<>(), result, new boolean[nums.length]);
+        return result;
+    }
+
+    private void backtrack11(int[] nums, List<Integer> path, List<List<Integer>> result, boolean[] used) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || set.contains(nums[i])) {
+                continue;
+            }
+            set.add(nums[i]);
+            path.add(nums[i]);
+            used[i] = true;
+            backtrack11(nums, path, result, used);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
 
     public static void main(String[] args) {
-        int[] candidates = new int[]{2, 3, 6, 7};
+        int[] nums = new int[]{1, 1, 2};
         BackTrackCode backTrackCode = new BackTrackCode();
-        List<List<Integer>> result = backTrackCode.combinationSum(candidates, 7);
+        List<List<Integer>> result = backTrackCode.permuteUnique(nums);
         System.out.println(result);
     }
 
